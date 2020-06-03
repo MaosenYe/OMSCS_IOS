fc0e8711cf94b650fc27e681ad06debbff25bad3
もぃ <30550196+MaosenYe@users.noreply.github.com>
2020-06-03 16:27:42 +0800

    add makefile and add CLI for client

--- Project 1/echo/client.c
+++ Project 1/echo/client.c
@@ -1,57 +1,153 @@
+/* client.c */
+/* 0  Copyright (c) 2020 もぃ. All rights reserved. */
+/* 1 includes */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <libgen.h>
+#include <errno.h>
 #include <string.h>
+#include <getopt.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
+/* 2 defines */ 
+#define USAGE                                                                       \
+    "usage:\n"                                                                      \
+    "  echoclient [options]\n"                                                      \
+    "options:\n"                                                                    \
+    "  -s                  Server (Default: localhost)\n"                           \
+    "  -p                  Port (Default: 20502)\n"                                  \
+    "  -m                  Message to send to server (Default: \"Hello world.\")\n" \
+    "  -h                  Show this help message\n"
+#define OPTSTR "vi:o:f:h"
+#define USAGE_FMT  "%s [-s Server] [-p Port] [-m Message] [-h]\n"
+#define ERR_SEND_SOCKET "send sockets"
+#define DEFAULT_PROGNAME "moli"
+#define BUFSIZE 256
+
+/* 3 external declarations */
+extern int errno;
+extern char *optarg;
+extern int opterr, optind;
+
+/* 4 typedefs */
+typedef struct {
+  char          *hostname;
+  int           portno;
+  char          *message;
+} options_t;
+
+/* 5 global variable declarations */
+/* 6 function prototypes */
+void usage(char *progname, int opt);
+int  send_sockets(options_t *options);
+
+/* 7 main function  */
+int main(int argc, char *argv[]) {
+    int opt;
+    options_t *options;
+
+    opterr = 0;
+
+    while ((opt = getopt(argc, argv, OPTSTR)) != EOF)
+       switch(opt) {
+           case 's':
+                options->hostname=optarg;
+                break;
+
+           case 'p':
+                options->portno=atoi(optarg);
+                break;
+
+           case 'm':
+                options->message=optarg;
+                break;               
+
+           case 'h':
+              fprintf(stdout, "%s", USAGE);
+              exit(0);
+              break;
+
+           default:
+              usage(basename(argv[0]), opt);
+              /* NOTREACHED */
+              break;
+       }
+
+    if (send_sockets(&options) != EXIT_SUCCESS) {
+       perror(ERR_SEND_SOCKET);
+       exit(EXIT_FAILURE);
+       /* NOTREACHED */
+    }
+
+    return EXIT_SUCCESS;
+}  
+
+/* 8 function declarations */
+void usage(char *progname, int opt) {
+   fprintf(stderr, USAGE_FMT, progname?progname:DEFAULT_PROGNAME);
+   exit(EXIT_FAILURE);
+   /* NOTREACHED */
 }
 
-int main(int argc, char *argv[])
-{
-    int sockfd, portno, n;
+int send_sockets(options_t *options) {
+
+   if (!options) {
+     errno = EINVAL;
+     return EXIT_FAILURE;
+   }
+
+   if (options->hostname==NULL||options->portno==NULL) {
+     errno = EDESTADDRREQ;
+     return EXIT_FAILURE;
+   }
+
+   if (options->message==NULL) {
+     errno = ENODATA;
+     return EXIT_FAILURE;
+   }
+
+    int sockfd;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
+    char buffer[BUFSIZE];
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
+    if (sockfd < 0){
+     errno=ESOCKTNOSUPPORT;
+     return EXIT_FAILURE;
+    }
+    server = gethostbyname(options->hostname);
     if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+     errno=EHOSTUNREACH;
+     return EXIT_FAILURE;
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, 
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
+    serv_addr.sin_port = htons(options->portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
+     errno=ENOTCONN;
+     return EXIT_FAILURE;        
+    }
+    bzero(buffer,BUFSIZE);
+    strcpy((char*) buffer , options->message);
+    int n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0){
+     errno=EBADMSG;
+     return EXIT_FAILURE;
+    } 
+    bzero(buffer,BUFSIZE);
+    n = read(sockfd,buffer,BUFSIZE);
+    if (n < 0){
+     errno=EBADMSG;
+     return EXIT_FAILURE;
+    } 
     printf("%s\n",buffer);
     close(sockfd);
-    return 0;
+    return EXIT_SUCCESS;
 }
---
+++ temple.c
@@ -0,0 +1,119 @@
+/* main.c */
+/* 0 copyright/licensing */
+/* 1 includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <errno.h>
+#include <string.h>
+#include <getopt.h>
+#include <sys/types.h>
+/* 2 defines */
+#define USAGE                                                                       \
+    "usage:\n"                                                                      \
+    "  echoclient [options]\n"                                                      \
+    "options:\n"                                                                    \
+    "  -s                  Server (Default: localhost)\n"                           \
+    "  -p                  Port (Default: 20502)\n"                                  \
+    "  -m                  Message to send to server (Default: \"Hello world.\")\n" \
+    "  -h                  Show this help message\n"
+#define OPTSTR "vi:o:f:h"
+#define USAGE_FMT  "%s [-v] [-f hexflag] [-i inputfile] [-o outputfile] [-h]\n"
+#define ERR_FOPEN_INPUT  "fopen(input, r)"
+#define ERR_FOPEN_OUTPUT "fopen(output, w)"
+#define ERR_DO_THE_NEEDFUL "do_the_needful blew up"
+#define DEFAULT_PROGNAME "moli"
+
+/* 3 external declarations */
+extern int errno;
+extern char *optarg;
+extern int opterr, optind;
+/* 4 typedefs */
+typedef struct {
+  int           verbose;
+  uint32_t      flags;
+  FILE         *input;
+  FILE         *output;
+} options_t;
+/* 5 global variable declarations */
+/* 6 function prototypes */
+void usage(char *progname, int opt);
+int  do_the_needful(options_t *options);
+
+/* 7 main function  */
+int main(int argc, char *argv[]) {
+    int opt;
+    options_t options = { 0, 0x0, stdin, stdout };
+
+    opterr = 0;
+
+    while ((opt = getopt(argc, argv, OPTSTR)) != EOF)
+       switch(opt) {
+           case 'i':
+              if (!(options.input = fopen(optarg, "r")) ){
+                 perror(ERR_FOPEN_INPUT);
+                 exit(EXIT_FAILURE);
+                 /* NOTREACHED */
+              }
+              break;
+
+           case 'o':
+              if (!(options.output = fopen(optarg, "w")) ){
+                 perror(ERR_FOPEN_OUTPUT);
+                 exit(EXIT_FAILURE);
+                 /* NOTREACHED */
+              }    
+              break;
+             
+           case 'f':
+              options.flags = (uint32_t )strtoul(optarg, NULL, 16);
+              break;
+
+           case 'v':
+              options.verbose += 1;
+              break;
+
+           case 'h':
+              fprintf(stdout, "%s", USAGE);
+              exit(0);
+              break;
+
+           default:
+              usage(basename(argv[0]), opt);
+              /* NOTREACHED */
+              break;
+       }
+
+    if (do_the_needful(&options) != EXIT_SUCCESS) {
+       perror(ERR_DO_THE_NEEDFUL);
+       exit(EXIT_FAILURE);
+       /* NOTREACHED */
+    }
+
+    return EXIT_SUCCESS;
+}
+
+/* 8 function declarations */
+void usage(char *progname, int opt) {
+   fprintf(stderr, USAGE_FMT, progname?progname:DEFAULT_PROGNAME);
+   exit(EXIT_FAILURE);
+   /* NOTREACHED */
+}
+
+int do_the_needful(options_t *options) {
+
+   if (!options) {
+     errno = EINVAL;
+     return EXIT_FAILURE;
+   }
+
+   if (!options->input || !options->output) {
+     errno = ENOENT;
+     return EXIT_FAILURE;
+   }
+
+   /* XXX do needful stuff */
+
+   return EXIT_SUCCESS;
+}
